5a910ad3f9dd8d89ba955b02a1b09ca57677ed3e
Roman Bobrovskiy <romul1988@gmail.com>
2026-01-07 11:51:36 +0300

    initial

---
+++ handlers_gen/codegen.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"log"
+	"os"
+)
+
+func main() {
+	fset := token.NewFileSet()
+	node, err := parser.ParseFile(fset, os.Args[1], nil, parser.ParseComments)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	out, _ := os.Create(os.Args[2])
+
+	fmt.Fprintln(out, `package `+node.Name.Name)
+	fmt.Fprintln(out)
+
+	for _, f := range node.Decls {
+		g, ok := f.(*ast.GenDecl)
+		if !ok {
+			fmt.Printf("SKIP %#T is not *ast.GenDecl\n", f)
+			continue
+		}
+		// SPECS_LOOP:
+		for _, spec := range g.Specs {
+			currType, ok := spec.(*ast.TypeSpec)
+			if !ok {
+				fmt.Printf("SKIP %#T is not ast.TypeSpec\n", spec)
+				continue
+			}
+			fmt.Println(currType)
+		}
+
+	}
+
+}
